440ac682a733c4d082ee9fa255887a2d9457ae01
shriya7756 <shriyapachunuri@gmail.com>
2026-06-22 23:46:53 +0530

    fix: switch to static export for netlify deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
   reactCompiler: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
